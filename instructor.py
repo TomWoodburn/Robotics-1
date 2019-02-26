6cfa7ba001ac829ce197158dcedd69895b1c840b
Nirav <32882197+NiravGC@users.noreply.github.com>
2019-02-26 11:30:19 +0000

    Update instructor.py

--- instructor.py
+++ instructor.py
@@ -5,7 +5,7 @@
 #                                                   #
 #       Test functionality of individual arms       #
 #                  and run full demo                #
-#  			                            #
+#                                                   #
 # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # brick dims: 0.2 x 0.09 x 0.062 metres
@@ -33,17 +33,17 @@ from std_msgs.msg import (
 import baxter_interface
 
 function_dict = {
-	1: 'calibrate',
-	2: 'demo',
-	3: 'move to cpose',
-	4: 'open',
-	5: 'close',
-	6: 'hover brick',
-	7: 'pick brick',
-	8: 'move to center',
-	9: 'release brick',
-	10: 'manual move',
-	11: 'angles'
+    1: 'calibrate',
+    2: 'demo',
+    3: 'move to cpose',
+    4: 'open',
+    5: 'close',
+    6: 'hover brick',
+    7: 'pick brick',
+    8: 'move to center',
+    9: 'release brick',
+    10: 'manual move',
+    11: 'angles'
 }
 
 instruction_message = "Please enter one of the following commands: \n\
@@ -62,27 +62,27 @@ instruction_message = "Please enter one of the following commands: \n\
 print(instruction_message)
 
 def instruction():
-	pub = rospy.Publisher('instructor', String, queue_size=10)
-	rospy.init_node('instruction_terminal', anonymous=True)
-	while not rospy.is_shutdown():
-		command_raw = raw_input("ENTER COMMAND: ")
-		if len(command_raw) < 3:
-			command_num = int(command_raw)
-			if command_num in function_dict:
-				command = function_dict[command_num]
-			else:
-				print("Number does not correlate with a command")
-				continue
-		elif command_raw in function_dict.values():
-			command = command_raw
-		else:
-			print("Invalid function")
-			continue
-		# print("ISSUING COMMAND: {}".format(command))
-		pub.publish(command)
+    pub = rospy.Publisher('instructor', String, queue_size=10)
+    rospy.init_node('instruction_terminal', anonymous=True)
+    while not rospy.is_shutdown():
+        command_raw = raw_input("ENTER COMMAND: ")
+        if len(command_raw) < 3:
+            command_num = int(command_raw)
+            if command_num in function_dict:
+                command = function_dict[command_num]
+            else:
+                print("Number does not correlate with a command")
+                continue
+        elif command_raw in function_dict.values():
+            command = command_raw
+        else:
+            print("Invalid function")
+            continue
+        # print("ISSUING COMMAND: {}".format(command))
+        pub.publish(command)
 
 if __name__ == '__main__':
-	try:
-		instruction()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+        instruction()
+    except rospy.ROSInterruptException:
+        pass
