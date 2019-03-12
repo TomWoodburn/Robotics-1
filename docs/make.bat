79fe0d98ba9a4d990f4a1f8b2a9981fd1bb6a6a1
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2019-03-12 17:29:38 +0000

    New version of readthedocs
    New theme
    Template files
    Basic structure

--- docs/make.bat
+++ docs/make.bat
@@ -25,6 +25,13 @@ if errorlevel 9009 (
 	exit /b 1
 )
 
+if "%1" == "livehtml" (
+        %SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%/html
+        sphinx-autobuild -b html %ALLSPHINXOPTS% %BUILDDIR%/html
+        if errorlevel 1 exit /b 1
+        goto end
+)
+
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
 goto end
 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,7 +74,13 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+
+#html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -171,3 +177,15 @@ epub_title = project
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
+
+
+# BEN ADDED MARKDOWN ACCESS
+
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
+
